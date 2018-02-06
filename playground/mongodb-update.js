d1d3b6c7e5888f3994ec88398f681d06f7cc56ba
Jose Menes <jmenes1729@gmail.com>
2018-02-06 14:56:50 -0500

    Added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5a78e42c2b30b0ffdcff5881')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5a78d1b62b30b0ffdcff55d1')
+  }, {
+    $set: {
+      name: 'Doctor cab1729'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
