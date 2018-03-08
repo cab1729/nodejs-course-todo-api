95b05610b4c10a7b0de94ff1f924e35ef0c25ed7
Jose Menes <jmenes1729@gmail.com>
2018-03-08 14:02:08 -0500

    Added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -215,7 +215,7 @@ describe('POST /users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -233,7 +233,7 @@ describe('POST /users', () => {
 
   it('Should not create user with duplicate email', (done) => {
 
-    var email = 'thedoctor@tardis.com';
+    var email = 'cab1729@basicallyrun.com';
     var password = 'basicallyrun';
 
     request(app)
@@ -241,6 +241,60 @@ describe('POST /users', () => {
       .send({email, password})
       .expect(400)
       .end(done);
+
+  });
+});
+
+
+describe('POST /users/login', () => {
+  it('Should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+
+  });
+
+  it('Should reject invalid login', (done) => {
+    var email = users[1].email;
+    var password = 'badfuckingpassword';
+
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: password
+      })
+      .expect(401)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+      });
+    done();
 
   });
 });
